85ae6fb3d51fdf17961b697ce3b34b17a42b9e21
JudithHoyuelos <judith.hoyuelos@gmail.com>
2026-05-04 17:59:24 +0200

    limpieza de archivos

--- src/app/bienvenida/webapp/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Webapp() {
-    return (
-        <div className="h-screen w-screen flex items-center justify-center bg-violet-700">
-            This is not the webapp page! Please, update the url.
-        </div>
-    )
-}
